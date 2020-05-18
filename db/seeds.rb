319acfa813748f4674f4046a5a842800ff893e02
Seanbon0611 <seand0611@gmail.com>
2020-05-18 15:25:24 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Artist.create(name: "Armin Van Buuren", bio: "A DJ/Producer from the Netherlands.")
+# Artist.create(name: "Drake", bio: "A canadian rapper that was in a TV show where he played a guy in a wheelchair.")
+# Genre.create(name: "Trance")
+# Genre.create(name: "Rap")
+# Song.create(name: "This Is What It Feels Like", artist_id: 1, genre_id: 1)
+# Song.create(name: "Tootsie Slide", artist_id: 2, genre_id: 2)
