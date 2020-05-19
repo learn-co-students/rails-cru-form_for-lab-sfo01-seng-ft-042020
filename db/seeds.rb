6843cc5650e385660ed79ed2e3c74a34ec4bb997
James <jmcelroy@plexustech.net>
2020-05-19 18:59:00 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    lady_gaga = Artist.create(name:"Lady Gaga", bio:"Artsy queen")
+    tupac = Artist.create(name:"Tupac", bio:"thug rap")
+
+    pop = Genre.create(name:"Pop")
+    rap = Genre.create(name:"Rap")
+
+    california_dreamin = Song.create(name:"California Dreamin", artist_id: tupac.id, genre_id: rap.id)
+    poker_face = Song.create(name:"PokerFace", artist_id: lady_gaga.id, genre_id: pop.id)
+
+    
